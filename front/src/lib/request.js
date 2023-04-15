e319910e4787083d3b6af61886a32099fce1e918
o-ogie <112533540+o-ogie@users.noreply.github.com>
2023-04-15 17:31:05 +0900

    Update request.js
    baseURL 수정 기존 : 127.0.0.1 => api.hanjin.shop

--- front/src/lib/request.js
+++ front/src/lib/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const request = axios.create({
-    // baseURL: "https://api.hanjin.shop",
-    baseURL: "127.0.0.1:3001",
+    baseURL: "https://api.hanjin.shop",
+    // baseURL: "127.0.0.1:3001",
     withCredentials: true,
 });
 
